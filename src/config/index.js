6558cc20d70bc5d769acee12d6730806500429af
Adrian Guerrero <adrian@foostudio.mx>
2018-02-22 16:26:39 -0600

    refactor(src/config): - remove URLS const - update config.environments urls - fix environments validations - clean config object

--- src/config/index.js
+++ src/config/index.js
@@ -1,147 +1,140 @@
+import { isEqual } from "lodash";
+
 /**
- * Created by mendieta on 1/20/16.
+ * @param {String} path
  */
-
-const URLS = {
-    staging: {
-        url: 'http://www.fooprojects.com',
-        subdirectory: '/clients/brand/site',
-        public: '/clients/brand/site/static',
-        api: '/clients/brand/site/api',
-    },
-    qa: {
-        url: 'http://www.fooprojects.com',
-        subdirectory: '/clients/foo/boilerplate',
-        public: '/clients/foo/boilerplate/static',
-        api: '/clients/foo/boilerplate/api',
-    },
-    production: {
-        url: 'http://www.brand.com',
-        subdirectory: '',
-        public: '/static',
-        api: '/api',
-    },
-    development: {
-        url: 'http://localhost:8080',
-        subdirectory: '',
-        public: '/static',
-        api: '/api',
-    },
-};
-
-// CONFIG
-export const config = {
-    "locale": "es-MX",
-    "data_loading": true,
-    "asset_loading": true,
-    "facebook_permissions": "email",
-    "google_scopes": "https://www.googleapis.com/auth/plus.login",
-    "sdks": [],
-    "vars": {
-        "animate": true,
-        "resize": true
-    },
-    "urls": {},
-    "environments": {
-        "production": {
-            "vars": {
-                "base": URLS.production.url,
-                "route": URLS.production.subdirectory,
-                "public": URLS.production.public,
-                "debug": false,
-            },
-            "urls": {
-                "api": URLS.production.api,
-            },
-            "analytics": [
-                {
-                    "adapter": "google",
-                    "id": "uaXXXXXXXX"
-                }
-            ],
-            "properties": {
-                "fb": "XXXXXXXXXXXXXXX",
-                "gp": "XXXXXXXXXXXXXXX.apps.googleusercontent.com",
-                "xeerpa": "",
-            }
-        },
-        "staging": {
-            "vars": {
-                "base": URLS.staging.url,
-                "route": URLS.staging.subdirectory,
-                "public": URLS.staging.public,
-                "debug": true
-            },
-            "urls": {
-                "api": URLS.staging.api,
-            },
-            "analytics": [
-                {
-                    "adapter": "google",
-                    "id": "uaXXXXXXXX"
-                }
-            ],
-            "properties": {
-                "fb": "XXXXXXXXXXXXXXX",
-                "gp": "XXXXXXXXXXXXXXX.apps.googleusercontent.com",
-                "xeerpa": "",
-            }
-        },
-        "qa": {
-            "vars": {
-                "base": URLS.qa.url,
-                "route": URLS.qa.subdirectory,
-                "public": URLS.qa.public,
-                "debug": true
-            },
-            "urls": {
-                "api": URLS.qa.api,
-            },
-            "analytics": [
-                {
-                    "adapter": "google",
-                    "id": "uaXXXXXXXX"
-                }
-            ],
-            "properties": {
-                "fb": "XXXXXXXXXXXXXXX",
-                "gp": "XXXXXXXXXXXXXXX.apps.googleusercontent.com",
-                "xeerpa": "",
-            }
-        },
-        "development": {
-            "vars": {
-                "base": URLS.development.url,
-                "route": URLS.development.subdirectory,
-                "public": URLS.development.public,
-                "debug": true
-            },
-            "urls": {
-                "api": URLS.development.api
-            },
-            "analytics": [
-                {
-                    "adapter": "google",
-                    "id": "uaXXXXXXXX"
-                }
-            ],
-            "properties": {
-                "fb": "XXXXXXXXXXXXXXX",
-                "gp": "XXXXXXXXXXXXXXX.apps.googleusercontent.com",
-                "xeerpa": "",
-            }
-        }
-    }
-};
-
-// ENVIRONMENT
-function getHost(value) {
-    const url = new URL(value);
-    return url.host;
+function getPathValues(path) {
+    return path
+        .split("/")
+        .filter((val, i) => val !== "" || (val === "" && i === 0));
 }
 
-const env = Object
-    .entries(URLS)
-    .filter(current => getHost(current[1].url) === location.host)[0][0];
+/**
+ * @param {String} pathA
+ * @param {String} pathB
+ * @returns {Boolean}
+ */
+function equalPaths(pathA, pathB) {
+    const a = getPathValues(pathA);
+    const b = getPathValues(pathB);
+    return isEqual(a, b);
+}
 
-export const environment = config["environments"][env];
+export const config = Object.freeze({
+    locale: "es-MX",
+    data_loading: true,
+    asset_loading: true,
+    facebook_permissions: "email",
+    google_scopes: "https://www.googleapis.com/auth/plus.login",
+    sdks: [],
+    vars: {
+        animate: true,
+        resize: true,
+    },
+    urls: {},
+    environments: {
+        production: {
+            vars: {
+                debug: false,
+            },
+            url: {
+                base: "http://www.brand.com",
+                subdirectory: "",
+                public: "/static",
+                api: "/api",
+            },
+            analytics: [
+                {
+                    adapter: "google",
+                    id: "uaXXXXXXXX",
+                },
+            ],
+            properties: {
+                fb: "XXXXXXXXXXXXXXX",
+                gp: "XXXXXXXXXXXXXXX.apps.googleusercontent.com",
+                xeerpa: "",
+            },
+        },
+        staging: {
+            vars: {
+                debug: true,
+            },
+            url: {
+                base: "http://fooprojects.com",
+                subdirectory: "/clients/brand/site",
+                public: "/clients/brand/site/static",
+                api: "/clients/brand/site/api",
+            },
+            analytics: [
+                {
+                    adapter: "google",
+                    id: "uaXXXXXXXX",
+                },
+            ],
+            properties: {
+                fb: "XXXXXXXXXXXXXXX",
+                gp: "XXXXXXXXXXXXXXX.apps.googleusercontent.com",
+                xeerpa: "",
+            },
+        },
+        qa: {
+            vars: {
+                debug: true,
+            },
+            url: {
+                base: "http://fooprojects.com",
+                subdirectory: "/clients/foo/boilerplate",
+                public: "/clients/foo/boilerplate/static",
+                api: "/clients/foo/boilerplate/api",
+            },
+            analytics: [
+                {
+                    adapter: "google",
+                    id: "uaXXXXXXXX",
+                },
+            ],
+            properties: {
+                fb: "XXXXXXXXXXXXXXX",
+                gp: "XXXXXXXXXXXXXXX.apps.googleusercontent.com",
+                xeerpa: "",
+            },
+        },
+        development: {
+            vars: {
+                debug: true,
+            },
+            url: {
+                base: "http://localhost:8080",
+                subdirectory: "",
+                public: "/static",
+                api: "/api",
+            },
+            analytics: [
+                {
+                    adapter: "google",
+                    id: "uaXXXXXXXX",
+                },
+            ],
+            properties: {
+                fb: "XXXXXXXXXXXXXXX",
+                gp: "XXXXXXXXXXXXXXX.apps.googleusercontent.com",
+                xeerpa: "",
+            },
+        }
+    }
+});
+
+export const environment = Object
+    .entries(config.environments)
+    .reduce((prev, [key, env]) => {
+        if (Array.isArray(prev)) {
+            const url = new URL(`${env.url.base}${env.url.subdirectory}`);
+            // Compare hosts and paths. `equalPaths` compares if paths like
+            // '/foo/bar' and '/foo/bar/' (note the the last '/') are the same.
+            // Comparing url.href with location.href won't work in some cases.
+            const isSameHref = url.host === location.host && equalPaths(url.pathname, location.pathname);
+            // Default is development env
+            if (isSameHref || key === 'development') return env;
+        }
+        return prev;
+    }, []);
