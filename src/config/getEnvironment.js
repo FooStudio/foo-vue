271f0ca12039cbacae3a4c0ffa82f9affbf81baf
Adrian Guerrero <adrian@foostudio.mx>
2018-02-28 19:08:37 -0600

    feat(src/config): set default and custom config

---
+++ src/config/getEnvironment.js
@@ -0,0 +1,38 @@
+import { isEqual } from "lodash";
+
+/**
+ * @param {String} path
+ */
+function getPathValues(path) {
+    return path
+        .split("/")
+        .filter((val, i) => val !== "" || (val === "" && i === 0));
+}
+
+/**
+ * @param {String} pathA
+ * @param {String} pathB
+ * @returns {Boolean}
+ */
+function equalPaths(pathA, pathB) {
+    const a = getPathValues(pathA);
+    const b = getPathValues(pathB);
+    return isEqual(a, b);
+}
+
+export default function(environments) {
+    return Object
+    .entries(environments)
+    .reduce((prev, [key, env]) => {
+        if (Array.isArray(prev)) {
+            const url = new URL(`${env.url.base}${env.url.subdirectory}`);
+            // Compare hosts and paths. `equalPaths` compares if paths like
+            // '/foo/bar' and '/foo/bar/' (note the the last '/') are the same.
+            // Comparing url.href with location.href won't work in some cases.
+            const isSameHref = url.host === location.host && equalPaths(url.pathname, location.pathname);
+            // Default is development env
+            if (isSameHref || key === 'development') return env;
+        }
+        return prev;
+    }, []);
+}
--- src/config/index.js
+++ src/config/index.js
@@ -1,140 +1,96 @@
-import { isEqual } from "lodash";
+import DEFAULT_CONFIG from "./defaultConfig";
+import getEnvironment from "./getEnvironment";
+import { defaultsDeep } from "lodash";
 
 /**
- * @param {String} path
+ * @typedef EnvironmentVars
+ * @prop {Boolean} debug
+ *
+ * @typedef EnvironmentUrl
+ * @prop {String} base
+ * @prop {String} subdirectory
+ * @prop {String} public
+ * @prop {String} api
+ *
+ * @typedef EnvironmentAnalytics
+ * @prop {String} adapter
+ * @prop {String} id
+ *
+ * @typedef EnvironmentProperties
+ * @prop {String} fb
+ * @prop {String} gp
+ * @prop {String} xeerpa
+ *
+ * @typedef Environment
+ * @prop {EnvironmentVars} vars
+ * @prop {EnvironmentUrl} url
+ * @prop {EnvironmentAnalytics[]} analytics
+ * @prop {EnvironmentProperties} properties
+ *
+ * @typedef ConfigEnvironments
+ * @prop {Environment} production
+ * @prop {Environment} staging
+ * @prop {Environment} qa
+ * @prop {Environment} development
+ *
+ * @typedef ConfigVars
+ * @prop {Boolean} animate
+ * @prop {Boolean} resize
+ *
+ * @typedef Config
+ * @prop {String} locale
+ * @prop {Boolean} data_loading
+ * @prop {Boolean} asset_loading
+ * @prop {String} facebook_permissions
+ * @prop {String} google_scopes
+ * @prop {String[]} sdks
+ * @prop {ConfigVars} vars
+ * @prop {ConfigEnvironments} environments
  */
-function getPathValues(path) {
-    return path
-        .split("/")
-        .filter((val, i) => val !== "" || (val === "" && i === 0));
-}
 
 /**
- * @param {String} pathA
- * @param {String} pathB
- * @returns {Boolean}
+ * @type {Config}
  */
-function equalPaths(pathA, pathB) {
-    const a = getPathValues(pathA);
-    const b = getPathValues(pathB);
-    return isEqual(a, b);
-}
-
-export const config = Object.freeze({
-    locale: "es-MX",
-    data_loading: true,
-    asset_loading: true,
-    facebook_permissions: "email",
-    google_scopes: "https://www.googleapis.com/auth/plus.login",
-    sdks: [],
-    vars: {
-        animate: true,
-        resize: true,
-    },
-    urls: {},
+const CUSTOM_CONFIG = {
     environments: {
         production: {
             vars: {
                 debug: false,
             },
             url: {
-                base: "http://www.brand.com",
-                subdirectory: "",
-                public: "/static",
-                api: "/api",
-            },
-            analytics: [
-                {
-                    adapter: "google",
-                    id: "uaXXXXXXXX",
-                },
-            ],
-            properties: {
-                fb: "XXXXXXXXXXXXXXX",
-                gp: "XXXXXXXXXXXXXXX.apps.googleusercontent.com",
-                xeerpa: "",
+                base: "http://brand.com",
             },
         },
         staging: {
-            vars: {
-                debug: true,
-            },
             url: {
                 base: "http://fooprojects.com",
                 subdirectory: "/clients/brand/site",
                 public: "/clients/brand/site/static",
                 api: "/clients/brand/site/api",
             },
-            analytics: [
-                {
-                    adapter: "google",
-                    id: "uaXXXXXXXX",
-                },
-            ],
-            properties: {
-                fb: "XXXXXXXXXXXXXXX",
-                gp: "XXXXXXXXXXXXXXX.apps.googleusercontent.com",
-                xeerpa: "",
-            },
         },
         qa: {
-            vars: {
-                debug: true,
-            },
             url: {
                 base: "http://fooprojects.com",
                 subdirectory: "/clients/foo/boilerplate",
                 public: "/clients/foo/boilerplate/static",
                 api: "/clients/foo/boilerplate/api",
             },
-            analytics: [
-                {
-                    adapter: "google",
-                    id: "uaXXXXXXXX",
-                },
-            ],
-            properties: {
-                fb: "XXXXXXXXXXXXXXX",
-                gp: "XXXXXXXXXXXXXXX.apps.googleusercontent.com",
-                xeerpa: "",
-            },
         },
         development: {
-            vars: {
-                debug: true,
-            },
             url: {
                 base: "http://localhost:8080",
-                subdirectory: "",
-                public: "/static",
-                api: "/api",
             },
-            analytics: [
-                {
-                    adapter: "google",
-                    id: "uaXXXXXXXX",
-                },
-            ],
-            properties: {
-                fb: "XXXXXXXXXXXXXXX",
-                gp: "XXXXXXXXXXXXXXX.apps.googleusercontent.com",
-                xeerpa: "",
-            },
-        }
-    }
-});
+        },
+    },
+};
 
-export const environment = Object
-    .entries(config.environments)
-    .reduce((prev, [key, env]) => {
-        if (Array.isArray(prev)) {
-            const url = new URL(`${env.url.base}${env.url.subdirectory}`);
-            // Compare hosts and paths. `equalPaths` compares if paths like
-            // '/foo/bar' and '/foo/bar/' (note the the last '/') are the same.
-            // Comparing url.href with location.href won't work in some cases.
-            const isSameHref = url.host === location.host && equalPaths(url.pathname, location.pathname);
-            // Default is development env
-            if (isSameHref || key === 'development') return env;
-        }
-        return prev;
-    }, []);
+/**
+ * @type {Config}
+ */
+export const config = defaultsDeep(CUSTOM_CONFIG, DEFAULT_CONFIG);
+
+/**
+ * @type {Environment}
+ */
+export const environment = getEnvironment(config.environments);
